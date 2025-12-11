382f9a08fbf27f4516cf4e42bddd6020a0403df2
philopian <philopian@gmail.com>
2025-12-11 11:04:46 -0800

    feat: added file path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -10,4 +11,12 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 8181,
+  },
 })
